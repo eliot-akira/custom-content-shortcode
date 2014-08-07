458573cbcb787d515ad163f50243a09b0432b7d4
Eliot Akira <me@eliotakira.com>
2014-08-07 05:47:44 +0200

    doc

--- includes/ccs-docs.php
+++ includes/ccs-docs.php
@@ -299,8 +299,8 @@ function ccs_content_settings_page() {
 				<div align="center">
 					<img src="<?php echo plugins_url();?>/custom-content-shortcode/docs/logo/logo.png"><br><br>
 					<b>Custom Content Shortcode</b> is developed by Eliot Akira.<br><br>
-					For general questions, please visit the <a href="http://wordpress.org/support/plugin/custom-content-shortcode" target="_blank">WordPress plugin support forum</a>.<br>
-					For commercial support and other inquiries, contact <a href="mailto:me@eliotakira.com">me@eliotakira.com</a><br>
+					Please visit the <a href="http://wordpress.org/support/plugin/custom-content-shortcode" target="_blank">WordPress plugin support forum</a> for general questions.<br>
+					For commercial support and development, contact <a href="mailto:me@eliotakira.com">me@eliotakira.com</a><br>
 				</div>
 				<?php
 			}
